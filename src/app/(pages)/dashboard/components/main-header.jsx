7429edd7ad622bc40a3571dbad43974bcdef89a4
Nareswara05 <naresalhaq9@gmail.com>
2024-05-14 08:04:00 +0700

    feat [NR] fixing missing icon in sidebar

--- src/app/(pages)/dashboard/components/main-header.jsx
+++ src/app/(pages)/dashboard/components/main-header.jsx
@@ -2,12 +2,14 @@
 
 import React from "react";
 import Image from "next/image";
-import { FiMenu } from "react-icons/fi";
+import { FiMenu, FiX } from "react-icons/fi"; 
+import { MdArrowBackIosNew } from "react-icons/md";
 import { useSidebar } from "./SidebarContext";
 import { IoIosArrowDown } from "react-icons/io";
 import { usePathname } from "next/navigation";
 import MenuProfileDashboard from "./main-profile";
 import { FiMail } from "react-icons/fi";
+
 export default function MainHeader() {
   const [isSideBarOpen, toggleSidebar] = useSidebar();
 
@@ -31,14 +33,12 @@ export default function MainHeader() {
 
   return (
     <div className="w-full h-[13%] bg-white flex flex-row justify-between items-center border-b-2 pr-8">
-      <div className="w-full flex flex-row items-center ">
+      <div className="w-full flex flex-row items-center">
         <div
-          className={`flex ml-8 cursor-pointer ${
-            isSideBarOpen ? "lg:hidden" : ""
-          }`}
+          className={`flex ml-8 cursor-pointer text-textPrimary`}
           onClick={handleMenuClick}
         >
-          <FiMenu size={30} />
+          {isSideBarOpen ? <MdArrowBackIosNew size={30} /> : <FiMenu size={30} />}
         </div>
 
         <h2 className="font-semibold font-montserrat text-lg lg:text-2xl pl-16 text-textPrimary ">
@@ -47,7 +47,7 @@ export default function MainHeader() {
         <div className="w-1/4 h-px ml-4 border border-textPrimary rounded-xl"></div>
       </div>
       <div className="flex items-center gap-8">
-          <MenuProfileDashboard/>
+        <MenuProfileDashboard />
       </div>
     </div>
   );
