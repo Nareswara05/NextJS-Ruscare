68e495c07f84f7985accaf51f7a5fca46fc3239d
Nareswara05 <naresalhaq9@gmail.com>
2024-05-15 22:15:34 +0700

    feat [NR] add history page and pagination

--- src/app/(pages)/dashboard/history/page.jsx
+++ src/app/(pages)/dashboard/history/page.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import TableHistory from './components/table-history'
 
 const HistoryPage = () => {
   return (
-    <div>HistoryPage</div>
+    <div className='px-12 pt-5'>
+      <TableHistory/>
+    </div>
   )
 }
 
