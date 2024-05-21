f16853708ddfd16717ed3e1f4a6d1a313d004e06
Nareswara05 <naresalhaq9@gmail.com>
2024-05-21 10:59:09 +0700

    feat [NR] fetch login api

--- src/app/(pages)/(initial-pages)/login/page.jsx
+++ src/app/(pages)/(initial-pages)/login/page.jsx
@@ -5,44 +5,9 @@ import { logoPurple } from "@/app/lib/utils/svg";
 import Form from "../components/form";
 import Image from "next/image";
 import Link from "next/link";
+import LoginForm from './form-layout';
 
 export default function LoginPage() {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    const [showPassword, setShowPassword] = useState(false);
-    const [errors, setErrors] = useState({});
-
-    const toggleShowPassword = () => {
-        setShowPassword(!showPassword);
-    };
-
-    const validateForm = () => {
-        let errors = {};
-        let isValid = true;
-
-        if (!email) {
-            errors.email = 'Email wajib diisi';
-            isValid = false;
-        } else if (!/\S+@\S+\.\S+/.test(email)) {
-            errors.email = 'Email tidak valid';
-            isValid = false;
-        }
-
-        if (!password) {
-            errors.password = 'Password wajib diisi';
-            isValid = false;
-        }
-
-        setErrors(errors);
-        return isValid;
-    };
-
-    const handleLogin = (e) => {
-        e.preventDefault();
-        if (validateForm()) {
-            console.log('Form submitted successfully');
-        }
-    };
 
     return (
         <main className="h-screen w-screen flex flex-col items-center px-8 py-10 lg:flex-row-reverse lg:py-0 lg:px-0 bg-white">
@@ -56,51 +21,8 @@ export default function LoginPage() {
                 />
             </div>
 
-            <div className="w-full h-full flex flex-col items-start lg:justify-between py-[5vh] lg:py-20 lg:px-32 lg:w-1/2">
-                <Image
-                    className=" items-end hidden lg:block"
-                    src={logoPurple}
-                    alt="Logo"
-                    width={115}
-                    height={50}
-                />
-                <div className="w-full ">
-                    <h1 className="text-black font-semibold text-2xl lg:text-[36px] mb-3">Masuk</h1>
-                    <h3 className="text-[#8D8D8D] font-light text-base mb-12">Selamat Datang Kembali!</h3>
-                    <div className="w-full flex flex-col gap-7 mb-5">
-                        <Form 
-                            label="Email" 
-                            value={email}
-                            onChange={(e) => setEmail(e.target.value)}
-                            error={errors.email}
-                        />
-                        <div className="relative">
-                            <Form
-                                label="Password"
-                                type={showPassword ? 'text' : 'password'}
-                                value={password}
-                                onChange={(e) => setPassword(e.target.value)}
-                                error={errors.password}
-                            />
-                            <button
-                                className="absolute top-12 text-3xl transform -translate-y-1/2 right-4 text-textPrimary focus:outline-none opacity-50"
-                                onClick={toggleShowPassword}
-                                type="button" 
-                            >
-                                {showPassword ? <AiOutlineEye /> : <AiOutlineEyeInvisible />}
-                            </button>
-                        </div>
-                        {errors.general && <p className="error text-red-700">{errors.general}</p>}
-                    </div>
-                </div>
-
-                <div className="w-full flex flex-col gap-5">
-                    <button type='submit' className="w-full bg-primary py-4 rounded-2xl text-white font-medium text-sm lg:text-lg" onClick={handleLogin}>Masuk</button>
-                    <div className="w-full flex gap-2 justify-center items-center">
-                        <h3 className="text-textPrimary text-xs lg:text-[16px]">Tidak mempunyai akun?</h3>
-                        <Link href="/register" className="text-primary font-semibold text-sm lg:text-[18px]">Daftar</Link>
-                    </div>
-                </div>
+            <div className="w-full h-full flex flex-col  items-center justify-center py-[5vh] lg:py-20 lg:px-32 lg:w-1/2">
+                <LoginForm/>
             </div>
         </main>
     );
