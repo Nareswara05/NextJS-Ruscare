0c15756f1a3920f4e0808fa64baef0cf411269b7
Nareswara05 <naresalhaq9@gmail.com>
2024-05-22 10:25:49 +0700

    feat [NR] add profile nav

---
+++ src/app/components/profile-nav.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from 'react'
+import { RxDashboard } from 'react-icons/rx';
+import { CiMail, CiLogout } from "react-icons/ci";
+import { AvatarTes } from '../lib/utils/image';
+import { IoIosArrowDown } from 'react-icons/io';
+import Image from 'next/image';
+import Link from 'next/link';
+import { GoPerson } from 'react-icons/go';
+
+
+const ProfileMenuNav = () => {
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsMenuOpen(!isMenuOpen);
+    };
+
+    return (
+        <div className='relative'>
+            <div className='flex gap-1 items-center cursor-pointer' onClick={toggleMenu}>
+                <Image
+                    src={AvatarTes}
+                    width={100}
+                    className='w-[44px] rounded-full'
+                />
+                <IoIosArrowDown className={`text-gray-400 text-2xl transition duration-200 ease-in-out ${isMenuOpen ? 'rotate-180' : ''}`} />
+            </div>
+
+            {isMenuOpen && (
+                <div className='absolute top-20 right-0 bg-white py-[20px] w-[226px] shadow-lg px-[10px] rounded-xl'>
+                    <ul>
+                        <li className='flex text-textPrimary gap-3 w-full hover:bg-gray-100 p-[10px] rounded-lg'>
+                            <Link href="/dashboard" className='flex gap-3'>
+                                <div className='text-xl'>
+                                    <RxDashboard />
+                                </div>
+                                Dashboard
+                            </Link>
+                        </li>
+                        <li className='flex text-textPrimary gap-3 w-full hover:bg-gray-100 p-[10px] rounded-lg'>
+                            <Link href="/profile" className='flex gap-3'>
+                                <div className='text-xl'>
+                                    <GoPerson />
+                                </div>
+                                Profil Saya
+                            </Link>
+                        </li>
+                        <li className='flex text-textPrimary gap-3 w-full hover:bg-gray-100 p-[10px] rounded-lg'>
+                            <Link href="/dashboard" className='flex gap-3'>
+                                <div className='text-xl'>
+                                    <CiMail  />
+                                </div>
+                                Mailbox
+                            </Link>
+                        </li>
+                        <li className='flex  gap-3 w-full text-red-600 hover:bg-gray-100 p-[10px] rounded-lg'>
+                            <Link href="/dashboard" className='flex gap-3'>
+                                <div className='text-xl'>
+                                    <CiLogout />
+                                </div>
+                                Logout
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            )}
+        </div>
+    );
+
+}
+
+export default ProfileMenuNav
