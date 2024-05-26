9a5376723a61853cf0f1b82d58f7c1e2780765bf
Nareswara05 <naresalhaq9@gmail.com>
2024-05-26 18:39:08 +0700

    feat [NR] add consultant page

--- src/app/(pages)/(feature-pages)/(feat)/consultant/page.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/consultant/page.jsx
@@ -1,8 +1,61 @@
 import React from 'react'
+import ConsultantCard from './components/consultant-card'
+import HeaderConsultant from './components/header-consultant';
 
 const ConsultantPage = () => {
+  const consultants = [
+    {
+      id: 1,
+      name: "John Doe",
+      pronouns: "(She / Her)",
+      education: "S1 Psikologi Universitas Indonesia",
+      major: "PPLG",
+      avatar: "/path/to/avatar1.jpg"
+    },
+    {
+      id: 2,
+      name: "Jane Smith",
+      pronouns: "(He / Him)",
+      education: "S2 Psikologi Universitas Gadjah Mada",
+      major: "Animasi 3D",
+      avatar: "/path/to/avatar2.jpg"
+    },
+    {
+      id: 3,
+      name: "Robert Johnson",
+      pronouns: "(He / Him)",
+      education: "S1 Psikologi Universitas Padjajaran",
+      major: "Design Grafis",
+      avatar: "/path/to/avatar3.jpg"
+    },
+    {
+      id: 4,
+      name: "Emily Davis",
+      pronouns: "(She / Her)",
+      education: "S1 Psikologi Universitas Airlangga",
+      major: "Animasi 2D",
+      avatar: "/path/to/avatar4.jpg"
+    },
+    {
+      id: 5,
+      name: "Michael Wilson",
+      pronouns: "(He / Him)",
+      education: "S1 Psikologi Universitas Brawijaya",
+      major: "Teknik Grafika",
+      avatar: "/path/to/avatar5.jpg"
+    }
+  ];
+
+  
   return (
-    <div>ConsultantPage</div>
+    <div className='bg-white py-24'>
+      <HeaderConsultant/>
+      <div className='flex flex-wrap justify-center gap-10 pt-8'>
+        {consultants.map((consultant) => (
+          <ConsultantCard key={consultant.id} consultant={consultant} />
+        ))}
+      </div>
+    </div>
   )
 }
 
