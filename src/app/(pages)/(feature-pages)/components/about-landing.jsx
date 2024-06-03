24ae7bbf9a7e1660e01fc2ceed6ef1d0c49b575d
Nareswara05 <naresalhaq9@gmail.com>
2024-06-03 14:34:50 +0700

    feat [NR] add about section landing page

---
+++ src/app/(pages)/(feature-pages)/components/about-landing.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+import { about } from '@/app/lib/utils/image'
+
+
+const AboutLanding = () => {
+  return (
+    <div className='px-[177px] flex gap-24'>
+        <div className='flex flex-col gap-2'>
+            <hr className='w-32 border-[5px] border-primary'/>
+            <h1 className='text-textPrimary font-bold text-[40px]'>Kenapa Harus RusCare?</h1>
+            <h1 className='font-normal text-[18px] text-textPrimary'>RusCare adalah pilihan utama untuk konseling siswa SMK Raden Umar Said karena fitur-fitur unggulannya. Dengan layanan booking, Selfcare, dan tes psikologi, RusCare memberikan akses mudah dan komprehensif untuk mendapatkan bimbingan yang dibutuhkan. Dibuat khusus untuk kebutuhan SMK Raden Umar Said, RusCare menjamin pengalaman konseling yang terfokus dan terpercaya bagi setiap siswa.</h1>
+        </div>
+        <Image
+        src={about}
+        />
+    </div>
+  )
+}
+
+export default AboutLanding
