5f056e119bdcc85d432202662003d83842c5d759
Nareswara05 <naresalhaq9@gmail.com>
2024-06-11 18:00:39 +0700

    feat [NR] format date article and format category detail article

--- src/app/(pages)/(feature-pages)/(feat)/article/[id]/components/detail-hero-article.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/article/[id]/components/detail-hero-article.jsx
@@ -1,7 +1,20 @@
 import React from 'react';
 import Image from 'next/image';
 
-export default async function DetailHeroArticle({articleData}) {
+const formatDate = (dateString) => {
+    const options = { day: 'numeric', month: 'long', year: 'numeric' };
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('id-ID', options).format(date);
+};
+
+
+export default async function DetailHeroArticle({articleData, categories}) {
+
+    const getCategoryName = (id) => {
+        const category = categories.find(category => category.id === id);
+        return category ? category.category_name : "Unknown Category";
+    };
+
     if (!articleData) {
         return <p>Article not found</p>;
       }
@@ -9,8 +22,8 @@ export default async function DetailHeroArticle({articleData}) {
         <div className='flex flex-col gap-3 '>
             <Image className='w-full rounded-lg h-[500px] object-cover bg-primary' width={1000} height={1000} src={articleData.featured_image} alt={articleData.title} />
             <div className='w-full flex justify-between items-center'>
-                <h1 className='py-2 px-3 bg-primary rounded-lg text-white'>{articleData.category_id}</h1>
-                <h2 className='text-textPrimary'>{articleData.created_at}</h2>
+                <h1 className='py-2 px-3 bg-primary rounded-lg text-white'>{getCategoryName(articleData.category_id)}</h1>
+                <h2 className='text-textPrimary font-semibold'>{formatDate(articleData.created_at)}</h2>
             </div>
         </div>
     );
--- src/app/(pages)/(feature-pages)/(feat)/article/[id]/page.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/article/[id]/page.jsx
@@ -1,10 +1,12 @@
 import listArticle from '@/app/lib/service/endpoint/api/list-article';
+import listCategoryArticle from '@/app/lib/service/endpoint/api/list-category-article';
 import React from 'react';
 import DetailHeroArticle from './components/detail-hero-article';
 import DetailMainArticle from './components/detail-main-article';
 
 export default async function DetailPageArticle({params}) {
     const articles = await listArticle();
+    const categories = await listCategoryArticle();
       
 
     const {id} = params;
@@ -17,7 +19,7 @@ export default async function DetailPageArticle({params}) {
     return (
         <div className=' flex flex-col gap-10 bg-white px-24 py-32'>
 
-            <DetailHeroArticle  articleData={articleDetail}/>
+            <DetailHeroArticle categories={categories} articleData={articleDetail}/>
             <DetailMainArticle  articleData={articleDetail}/>
         </div>
     );
