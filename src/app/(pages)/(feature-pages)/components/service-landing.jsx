a18434ec63ddc0f64636b93e5203fae19921f3d7
Nareswara05 <naresalhaq9@gmail.com>
2024-06-17 22:30:24 +0700

    feat [NR] add popup detail service

--- src/app/(pages)/(feature-pages)/components/service-landing.jsx
+++ src/app/(pages)/(feature-pages)/components/service-landing.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import CardService from './decoration/card-service'
-import Image from 'next/image'
-import { halfcircle } from '@/app/lib/utils/svg'
-import listService from '@/app/lib/service/endpoint/api/list-service'
+import React from 'react';
+import CardService from './decoration/card-service';
+import Image from 'next/image';
+import { halfcircle } from '@/app/lib/utils/svg';
+import listService from '@/app/lib/service/endpoint/api/list-service';
 
 export default async function ServiceLanding ()  {
-    const services = await listService()
+    const services = await listService();
     return (
         <div className='w-full p-36 bg-primary flex flex-col items-center justify-center relative'>
             <div className='absolute -left-5 top-80'>
@@ -17,8 +17,8 @@ export default async function ServiceLanding ()  {
                 <div className='w-28 h-28 bg-[#8800F4] rotate-45 opacity-20 rounded-lg'></div>
             </div>
             <Image
-            src={halfcircle}
-            className='absolute left-20 top-20'
+                src={halfcircle}
+                className='absolute left-20 top-20'
             />
             <div className='text-center'>
                 <h1 className='font-bold text-[36px]'>LAYANAN KONSULTASI</h1>
@@ -26,11 +26,9 @@ export default async function ServiceLanding ()  {
             </div>
             <div className='flex flex-wrap gap-8 pt-16 items-center justify-center'>
                 {services.map((item, index) => (
-                    <CardService key={index} title={item.name} />
+                    <CardService key={index} title={item.name} description={item.description} />
                 ))}
             </div>
         </div>
     )
 }
-
-
