c431ec6f9c9decf5399eb96f8533fb903de89a0d
Nareswara05 <naresalhaq9@gmail.com>
2024-06-20 11:07:53 +0700

    feat [NR] add search query in article page

--- src/app/(pages)/(feature-pages)/(feat)/article/components/search-article.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/article/components/search-article.jsx
@@ -1,31 +1,31 @@
 "use client"
-import React, {useEffect, useState} from 'react'
-import {useRouter} from "next/navigation";
-import {useDebounce} from "use-debounce";
+import React, { useEffect, useState } from 'react';
+import { useRouter } from "next/navigation";
+import { useDebounce } from "use-debounce";
 
 const SearchArticle = () => {
-    const router = useRouter()
+    const router = useRouter();
     const [searchQuery, setSearchQuery] = useState('');
     const [query] = useDebounce(searchQuery, 500);
 
     useEffect(() => {
-        if (searchQuery) {
-            router.push(`/article?search=${searchQuery}`, {scroll: false})
+        if (query) {
+            router.push(`/article?search=${query}`, { scroll: false });
         } else {
-            router.push(`/article`)
+            router.push(`/article`);
         }
     }, [query, router]);
 
-  return (
-    <div>
-        <input
-            className='w-[300px] h-10 p-3 text-sm text-textPrimary border-gray-400 border-2 rounded-full focus:outline-none focus:border-primary'
-            type="text"
-            placeholder='Apa yang sedang kamu cari?'
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}/>
-    </div>
-  )
+    return (
+        <div>
+            <input
+                className='w-[300px] h-10 p-3 text-sm text-textPrimary border-gray-400 border-2 rounded-full focus:outline-none focus:border-primary'
+                type="text"
+                placeholder='Apa yang sedang kamu cari?'
+                value={searchQuery}
+                onChange={(e) => setSearchQuery(e.target.value)} />
+        </div>
+    )
 }
 
-export default SearchArticle
+export default SearchArticle;
--- src/app/lib/service/endpoint/api/list-article.js
+++ src/app/lib/service/endpoint/api/list-article.js
@@ -1,8 +1,13 @@
 import instance from "../../instance/instance";
 
-export default async function listArticle(category = "") {
+export default async function listArticle(category = "", search = "") {
     try {
-        const url = category ? `/article/show-by/category/${category}` : "/article/list";
+        let url;
+        if (search) {
+            url = `article/search/${search}`;
+        } else {
+            url = category ? `/article/show-by/category/${category}` : "/article/list";
+        }
         const response = await instance.get(url);
         return response.data.data;
     } catch (error) {
