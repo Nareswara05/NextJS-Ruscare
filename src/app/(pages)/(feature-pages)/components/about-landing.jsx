c44ce18a958e6838f77fbc73c4feefa5e7566bda
Nareswara05 <naresalhaq9@gmail.com>
2024-07-24 13:13:59 +0700

    fix [NR] fix revision

--- src/app/(pages)/(feature-pages)/components/about-landing.jsx
+++ src/app/(pages)/(feature-pages)/components/about-landing.jsx
@@ -9,7 +9,7 @@ const AboutLanding = () => {
         <div className='flex flex-col gap-2'>
             <hr className='w-32 border-[5px] border-primary'/>
             <h1 className='text-textPrimary font-bold text-[40px]'>Kenapa Harus RusCare?</h1>
-            <h1 className='font-normal text-[18px] text-textPrimary'>RusCare adalah pilihan utama untuk konseling siswa SMK Raden Umar Said karena fitur-fitur unggulannya. Dengan layanan booking, Selfcare, dan tes psikologi, RusCare memberikan akses mudah dan komprehensif untuk mendapatkan bimbingan yang dibutuhkan. Dibuat khusus untuk kebutuhan SMK Raden Umar Said, RusCare menjamin pengalaman konseling yang terfokus dan terpercaya bagi setiap siswa.</h1>
+            <h1 className='font-normal text-[16px] text-justify text-textPrimary'>RusCare adalah pilihan utama untuk konseling siswa SMK Raden Umar Said karena fitur-fitur unggulannya. Dengan layanan booking, Selfcare, dan tes psikologi, RusCare memberikan akses mudah dan komprehensif untuk mendapatkan bimbingan yang dibutuhkan. Dibuat khusus untuk kebutuhan SMK Raden Umar Said, RusCare menjamin pengalaman konseling yang terfokus dan terpercaya bagi setiap siswa.</h1>
         </div>
         <Image
         src={about}
