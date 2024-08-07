931623c2ef26ecc33f4e4160d8e3bf3bfca46a9c
Nareswara05 <naresalhaq9@gmail.com>
2024-08-07 23:00:49 +0700

    feat [NR] fetch edit image profile

--- src/app/(pages)/(feature-pages)/(additional)/profile/edit-profile/components/change-profile-picture.jsx
+++ src/app/(pages)/(feature-pages)/(additional)/profile/edit-profile/components/change-profile-picture.jsx
@@ -1,20 +1,46 @@
-import React from 'react'
-import { AvatarTes } from '@/app/lib/utils/image'
-import { TbEdit } from "react-icons/tb";
-import Image from 'next/image'
+import React, { useState } from 'react';
+import Image from 'next/image';
+import { TbEdit } from 'react-icons/tb';
 
+const ChangeProfilePicture = ({ image, onImageChange }) => {
+    const [selectedImage, setSelectedImage] = useState(null);
 
-const ChangeProfilePicture = () => {
+    const handleImageUpload = (event) => {
+        const file = event.target.files[0];
+        if (file) {
+            const reader = new FileReader();
+            reader.onloadend = () => {
+                setSelectedImage(reader.result);
+                onImageChange(file); // Pass the file to the parent component
+            };
+            reader.readAsDataURL(file);
+        }
+    };
+
+    const imageUrl = selectedImage || `https://api.ruscarestudent.com/${image}`;
+
     return (
         <div className='relative w-fit h-fit'>
-            <Image
-                src={AvatarTes}
-                className="w-48 rounded-full object-cover"
-            />
-            <div className='p-3 text-xl bg-primary text-white absolute right-0 bottom-0 rounded-full'>
-                <TbEdit />
+            <div className='w-48 h-48 rounded-full border border-primary'>
+                <Image
+                    src={imageUrl}
+                    alt="Profile Picture"
+                    className=" rounded-full object-cover w-full h-full"
+                    width={500}
+                    height={500}
+                />
             </div>
-        </div>)
-}
+            <label className='p-3 text-xl bg-primary text-white absolute right-0 bottom-0 rounded-full cursor-pointer'>
+                <TbEdit />
+                <input
+                    type='file'
+                    accept='image/*'
+                    className='hidden'
+                    onChange={handleImageUpload}
+                />
+            </label>
+        </div>
+    );
+};
 
-export default ChangeProfilePicture
+export default ChangeProfilePicture;
