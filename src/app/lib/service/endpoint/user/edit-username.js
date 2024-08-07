66e9fb6f66126c49698b197c8f586af77decde2f
Nareswara05 <naresalhaq9@gmail.com>
2024-08-07 22:30:59 +0700

    feat [NR] fetch edit username

--- src/app/lib/service/endpoint/user/edit-username.js
+++ src/app/lib/service/endpoint/user/edit-username.js
@@ -1,9 +1,9 @@
 import instance from "../../instance/instance";
 
-export default async function editUsername({ id, name }) {
+export default async function editUsername({ id, username }) {
   try {
-    const response = await instance.post(`/auth/edit-user/${id}`, {
-      name: name,
+    const response = await instance.post(`/student/edit-username/${id}`, {
+      username: username,
     });
     return response.data;
   } catch (error) {
