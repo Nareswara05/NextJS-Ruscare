3b756577e8f35572ce7c4fb011ce82e60d74d41f
Nareswara05 <naresalhaq9@gmail.com>
2024-09-02 09:38:36 +0700

    feat [NR] fix revision

--- src/app/(pages)/(feature-pages)/(feat)/selfcare/page.jsx
+++ src/app/(pages)/(feature-pages)/(feat)/selfcare/page.jsx
@@ -1,20 +1,44 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import CardSelfcare from './components/card-selfcare'
 import HeaderSelfcare from './components/header-selfcare'
 import listSelfcare from '@/app/lib/service/endpoint/api/list-selfcare'
+import { ClipLoader } from 'react-spinners';
 
-export default async function page(){
-  const selfcare = await listSelfcare();
+export default function Page() {
+  const [selfcare, setSelfcare] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchSelfcare() {
+      try {
+        const data = await listSelfcare();
+        setSelfcare(data);
+      } catch (error) {
+        console.error('Failed to fetch selfcare:', error);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchSelfcare();
+  }, []);
 
 
   return (
     <div className='pb-24 bg-white overflow-x-hidden flex flex-col gap-20 justify-center items-center'>
       <HeaderSelfcare />
-      <div className='grid grid-cols-2 gap-6 items-center'>
-        {selfcare.map((data, index) => (
-          <CardSelfcare key={index} data={data} />
-        ))}
-      </div>
+      {loading ? (
+        <div className='flex justify-center items-center pt-24'>
+          <ClipLoader color="#123abc" loading={loading} size={50} />
+        </div>
+      ) : (
+        <div className='grid grid-cols-2 gap-6 items-center'>
+          {selfcare.map((data, index) => (
+            <CardSelfcare key={index} data={data} />
+          ))}
+        </div>
+      )}
     </div>
   )
 }
